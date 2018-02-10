f71bfe41f6132ada7bce139bb462719b8739310e
aahmad94 <aka5vm@virginia.edu>
2018-02-10 09:54:03 -0800

    remove console logs and add comments

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -22,33 +22,30 @@ class Api::LocationsController < ApplicationController
   end 
 
   def route_coordinates
-    # truck_coord = [params[:lat], params[:lon]]
-    truck_coords = params[:truck_coords] # array of tuples with lat/lon info fir truck location
-    bounds = [] # array of hashes with lat_min/lat_max/lon_min/lon_max bounds
+    # array of tuples with lat/lon info fir truck location
+    truck_coords = params[:truck_coords] 
+
+    # array of hashes with lat_min/lat_max/lon_min/lon_max bounds
+    bounds = [] 
     truck_coords.each do |truck_coord| 
       bounds << calculate_bounds(truck_coord)
     end
-    # ret = {}
+
     locations = []
-    bounds.each_index do |idx|
-      houses = Location.where("lat > ? AND lat < ? AND lon > ? AND lon < ?", bounds[idx][:lat_min], bounds[idx][:lat_max], bounds[idx][:lon_min], bounds[idx][:lon_max]).to_a
-      truck = truck_coords[idx]
-      truck_hash = Location.new(lat: truck[0], lon: truck[1])
-      # ret['truck'] = truck_hash
-      # ret['data'] = tuck.concat(houses)
-      locations << {truck: truck_hash, houses: houses}
-      # locations << houses
+    bounds.each_index do |bound|
+      houses = Location.where("lat > ? AND lat < ? AND lon > ? AND lon < ?",
+        bounds[bound][:lat_min], bounds[bound][:lat_max], bounds[bound][:lon_min], bounds[bound][:lon_max]).to_a
+      
+      truck_location = Location.new(lat: truck_coords[bound][0], lon: truck_coords[bound][1])
+      locations << { truck: truck_location, houses: houses }
     end
-    p "----LOCATIONS----"
-    p locations
+
     @locations = locations  
-    # render :index
     render :json => @locations
   end
 
   private 
-  	def calculate_bounds(coord = [50.97452, 5.86605])      
-
+  	def calculate_bounds      
       uri = URI.parse("https://api.tomtom.com/routing/1/calculateReachableRange/#{coord[0]},#{coord[1]}/json?key=9jFqlnjOFua3WGH7neAbCktFIatGIp6D&fuelBudgetInLiters=0.6&constantSpeedConsumptionInLitersPerHundredkm=70,14")
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
@@ -59,36 +56,22 @@ class Api::LocationsController < ApplicationController
       response = http.request(request)
       body = JSON.parse(response.body)
       boundaries = body["reachableRange"]["boundary"]
+
+      # create min/max boundaries from array of all lat & lon values--this provides a square perimeter around a given location.
       bounds = Hash.new 
       lat = []
       lon = []
+
       boundaries.each do |coord| 
           lat.push(coord["latitude"])
           lon.push(coord["longitude"])
       end 
+
       bounds[:lat_min] = lat.min
       bounds[:lat_max] = lat.max 
       bounds[:lon_min] = lon.min 
       bounds[:lon_max] = lon.max 
+
       bounds
-    end 
-
-  # def fetch_lat_and_lon
-  #   parsed_address = self.address.split(" ").join("%20")
-    
-  #   uri = URI.parse("https://api.tomtom.com/search/2/geocode/#{parsed_address}.json?key=9jFqlnjOFua3WGH7neAbCktFIatGIp6D")
-  #   http = Net::HTTP.new(uri.host, uri.port)
-  #   http.use_ssl = true
-  #   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-  #   request = Net::HTTP::Get.new(uri.request_uri)
-
-  #   response = http.request(request)
-  #   body = JSON.parse(response.body)
-  #   position = body["results"][0]["position"]
-
-  #   self.lat = position["lat"]
-  #   self.lon = position["lon"]
-  #   self.save!
-  # end 
+    end
 end
